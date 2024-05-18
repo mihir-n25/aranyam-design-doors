c38745a13732a3f291e8b5a6a67789e3a43d98de
mihir-n25 <mihirnebani@gmail.com>
2024-05-18 16:27:57 +0530

    work page started

--- src/pages/Secondary/anim2.js
+++ src/pages/Secondary/anim2.js
@@ -4,10 +4,12 @@ export const slideUp = {
     },
     open: (i) => ({
         y: "0%",
+        opacity : "100%",
         transition: {duration: 0.8, delay: 0.1}
     }),
     closed: {
         y: "100%",
+        opacity : "0%",
         transition: {duration: 0.8}
     }
 }
--- src/pages/Work.js
+++ src/pages/Work.js
@@ -3,7 +3,11 @@ import Main from '../components/Layout/Main'
 
 const Work = () => {
   return (
-    <Main>Work</Main>
+    <Main>
+      <h3>Models</h3>
+      <h1>External Doors Catalog</h1>
+      <h2>Innovative Technology and craftemenship</h2>
+    </Main>
   )
 }
 
