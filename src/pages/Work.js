5b7e08ccc6e1c622548e8a1dca0ab38ae34fe639
mihir-n25 <mihirnebani@gmail.com>
2024-05-30 15:05:34 +0530

    some changes

--- src/pages/Work.js
+++ src/pages/Work.js
@@ -1,44 +1,44 @@
 import React, { useState } from "react";
 import Main from "../components/Layout/Main";
-import "../components/Styles/WorkStyles.css";
+import "../components/Styles/WorkStyles.scss";
 
 const Work = () => {
   const workImages = [
     {
       src: "/images/Designer (1).jpeg",
-      title: "hi",
-      price: "₹1299",
-      description: "hello gates",
+      title: "DarkSide",
+      price: "From ₹1299",
+      description: "This model can be made in a combination of veneer and enamel of these colors",
     },
     {
       src: "/images/Designer (3).jpeg",
-      title: "hi",
-      price: "₹1299",
-      description: "hello gates",
+      title: "DarkSide",
+      price: "From ₹1299",
+      description: "This model can be made in a combination of veneer and enamel of these colors",
     },
     {
       src: "/images/Designer (4).jpeg",
-      title: "hi",
-      price: "₹1299",
-      description: "hello gates",
+      title: "DarkSide",
+      price: "From ₹1299",
+      description: "This model can be made in a combination of veneer and enamel of these colors",
     },
     {
       src: "/images/Designer (5).jpeg",
-      title: "hi",
-      price: "₹1299",
-      description: "hello gates",
+      title: "DarkSide",
+      price: "From ₹1299",
+      description: "This model can be made in a combination of veneer and enamel of these colors",
     },
     {
       src: "/images/Designer (6).jpeg",
-      title: "hi",
-      price: "₹1299",
-      description: "hello gates",
+      title: "DarkSide",
+      price: "From ₹1299",
+      description: "This model can be made in a combination of veneer and enamel of these colors",
     },
     {
       src: "/images/Designer (7).jpeg",
-      title: "hi",
-      price: "₹1299",
-      description: "hello gates",
+      title: "DarkSide",
+      price: "From ₹1299",
+      description: "This model can be made in a combination of veneer and enamel of these colors",
     },
   ];
 
@@ -79,7 +79,6 @@ const Work = () => {
           NOTE : Prices are indicated for the single-leaf version in the initial
           configuration. To calculate a different execution, additional options
           and delivery, please contact the manager - Tel: +381691788177
-          Whatsapp, Telegram
         </p>
         <h3>Models</h3>
         <h1>External Doors Catalog</h1>
@@ -145,11 +144,10 @@ const Work = () => {
         <img
           alt="image-door"
           src="/images/Designer (3).jpeg"
-          className="image03"
         />
         <h1>doors 003</h1>
-        <h3>from ₹1299</h3>
-        <p>descripton of door</p>
+        <h3>From ₹1299</h3>
+        <p>This model can be made in a combination of veneer and enamel of these colors</p>
       </div>
       <div className="imageComponents2">
         {workImages2.map((workImage, index) => {
@@ -179,29 +177,32 @@ const Work = () => {
           </div>
         )}
       </div>
-      <div className="3DPart">
-        <div className="3dImage">
-          <img
-          alt="3dImage"
-          src=""
-          />
-        </div>
         <div className="Gcolors">
-          <img 
+        <img 
           alt="colors-image"
-          src=""/>
+          src="/images/Enamel.jpg"/>
+          <h3>This model can be made in different versions according to your sizes. Our engineers and designers will help you choose the best option!</h3>
           <img 
           alt="doors-image"
-          src=""/>
+          src="/images/view-desktop.svg"/>
+          <h3>The unique construction technology of BURKOVSKY entrance doors is a barrier against all irritants that can disturb your comfort and harmony.</h3>
           <img 
           alt="construction-image"
-          src=""/>
+          src="/images/slice2.jpg"/>
         </div>
-        <div className="3dDetails">
+        <hr/>
+        <div className="threeDPart">
+        <div className="threeDImage">
+          <img
+          alt="3dImage"
+          src="https://optim.tildacdn.com/tild6537-3561-4066-b064-306431623962/-/format/webp/10000.png"
+          />
+        </div>
+        <div className="threeDetails">
           <h1>Originals 3D models for designers and architects</h1>
           <p>.max format, version 2024</p>
           <div className="hr"></div>
-          {/* <a href="/pdf/Dip.pdf" download="Dip.pdf">Link to Download</a> */}
+          <a>Link to Download</a>
         </div>
       </div>
     </Main>
@@ -209,3 +210,4 @@ const Work = () => {
 };
 
 export default Work;
+
