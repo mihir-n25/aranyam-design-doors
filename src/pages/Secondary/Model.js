91defc7c34bd1cb0207996ba9668bb6a0e5a7ed3
mihir-n25 <mihirnebani@gmail.com>
2024-06-20 15:07:21 +0530

    cursor thing done

--- src/pages/Secondary/Model.js
+++ src/pages/Secondary/Model.js
@@ -14,14 +14,29 @@ const Model = ({projects , modal}) => {
   const {active , index} = modal;
 
   const container = useRef(null)
+  const cursorAbout = useRef(null)
+  const cursorLabel = useRef(null)
 
   useEffect(() => {
     const moveContainerX = gsap.quickTo(container.current , "left" , {duration : 0.8 , ease : "power3"})
     const moveContainerY = gsap.quickTo(container.current , "top" , {duration : 0.8 , ease : "power3"})
+
+    const moveCursorAboutX = gsap.quickTo(cursorAbout.current , "left" , {duration : 0.5 , ease : "power3"})
+    const moveCursorAboutY = gsap.quickTo(cursorAbout.current , "top" , {duration : 0.5 , ease : "power3"})
+
+    const moveCursorLabelX = gsap.quickTo(cursorLabel.current , "left" , {duration : 0.45 , ease : "power3"})
+    const moveCursorLabelY = gsap.quickTo(cursorLabel.current , "top" , {duration : 0.45 , ease : "power3"})
+
   window.addEventListener("mousemove" , (e) => {
     const {clientX , clientY} = e;
     moveContainerX(clientX);
     moveContainerY(clientY);
+
+    moveCursorAboutX(clientX);
+    moveCursorAboutY(clientY);
+    
+    moveCursorLabelX(clientX);
+    moveCursorLabelY(clientY);
   })
   }, [])
   
@@ -44,8 +59,8 @@ const Model = ({projects , modal}) => {
           }
         </div>
     </motion.div>
-    <div className='cursorAbout'></div>
-    <div className='cursorLabel'>View</div>
+    <motion.div ref={cursorAbout} variants={scaleAnimation} initial={"initial"} animate={active ? "open" : "closed"} className='cursorAbout'></motion.div>
+    <motion.div ref={cursorLabel} variants={scaleAnimation} initial={"initial"} animate={active ? "open" : "closed"} className='cursorLabel'>View</motion.div>
    </>
   )
 }
