6d56f09ed4d7a64b89c03f562548556a631f38b3
mihir-n25 <mihirnebani@gmail.com>
2024-06-22 15:28:57 +0530

    error

--- src/components/Animation/Curve.jsx
+++ src/components/Animation/Curve.jsx
@@ -3,52 +3,77 @@ import "./Curve.css";
 import { useState } from "react";
 import { useEffect } from "react";
 
+const anim = (variants) => {
+  return {
+    variants,
+    initial: "initial",
+    animate: "enter",
+    exit: "exit",
+  };
+};
+
 export default function Curve({ children }) {
-  const [dimensions, setDimension] = useState({
-    height: 0,
-    width: 0,
+  const [dimensions, setDimensions] = useState({
+    width: null,
+    height: null,
   });
 
   useEffect(() => {
-    const resize = () => {
-      setDimension({
+    function resize() {
+      setDimensions({
         width: window.innerWidth,
         height: window.innerHeight,
       });
-    };
+    }
+
     resize();
+
     window.addEventListener("resize", resize);
-    return () => window.removeEventListener("resize", resize);
+
+    return () => {
+      window.removeEventListener("resize", resize);
+    };
   }, []);
 
-  const anim = (variants) => {
-    return {
-      initial: "initial",
-      animate: "enter",
-      exit: "exit",
-      variants,
-    };
-  };
-
   return (
-    <div className="page curve">
-      {dimensions.width > 0 && <SVG {...dimensions} />}
+    <div className="page curve" style={{ backgroundColor }}>
+      <div
+        style={{ opacity: dimensions.width == null ? 1 : 0 }}
+        className="background"
+      />
+
+
+      {dimensions.width != null && <SVG {...dimensions} />}
+
       {children}
     </div>
   );
 }
 
-const SVG = ({ width, height }) => {
+const SVG = ({ height, width }) => {
   const initialPath = `
+
         M0 300 
         Q${width / 2} 0 ${width} 300
         L${width} ${height + 300}
         Q${width / 2} ${height + 600} 0 ${height + 300}
         L0 0
+
     `;
+
+  const targetPath = `
+
+        M0 300
+        Q${width / 2} 0 ${width} 300
+        L${width} ${height}
+        Q${width / 2} ${height} 0 ${height}
+        L0 0
+
+    `;
+
   return (
-    <svg>
-      <path d={initialPath}></path>
-    </svg>
+    <motion.svg {...anim(translate)}>
+      <motion.path {...anim(curve(initialPath, targetPath))} />
+    </motion.svg>
   );
 };
