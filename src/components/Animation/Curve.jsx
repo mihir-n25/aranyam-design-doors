949a1a6d4cc48a0cd81113c5a86ef05810a27591
mihir-n25 <mihirnebani@gmail.com>
2024-06-22 15:21:27 +0530

    setting up svg

--- src/components/Animation/Curve.jsx
+++ src/components/Animation/Curve.jsx
@@ -1,27 +1,54 @@
-import {motion} from "framer-motion"
-import './Curve.css';
+import { motion } from "framer-motion";
+import "./Curve.css";
+import { useState } from "react";
+import { useEffect } from "react";
 
-export default function Curve({children}){
-    const anim = (variants) => {
-        return {
-            initial : "initial",
-            animate : "enter",
-            exit : "exit",
-            variants
-        }
-    }
+export default function Curve({ children }) {
+  const [dimensions, setDimension] = useState({
+    height: 0,
+    width: 0,
+  });
 
-    return (
-        <div className="page curve">
-            {children}
-        </div>
-    )
+  useEffect(() => {
+    const resize = () => {
+      setDimension({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+    resize();
+    window.addEventListener("resize", resize);
+    return () => window.removeEventListener("resize", resize);
+  }, []);
+
+  const anim = (variants) => {
+    return {
+      initial: "initial",
+      animate: "enter",
+      exit: "exit",
+      variants,
+    };
+  };
+
+  return (
+    <div className="page curve">
+      {dimensions.width > 0 && <SVG {...dimensions} />}
+      {children}
+    </div>
+  );
 }
 
-const SVG = () => {
-    return (
-        <svg>
-            <path d=''></path>
-        </svg>
-    )
-}
+const SVG = ({ width, height }) => {
+  const initialPath = `
+        M0 300 
+        Q${width / 2} 0 ${width} 300
+        L${width} ${height + 300}
+        Q${width / 2} ${height + 600} 0 ${height + 300}
+        L0 0
+    `;
+  return (
+    <svg>
+      <path d={initialPath}></path>
+    </svg>
+  );
+};
